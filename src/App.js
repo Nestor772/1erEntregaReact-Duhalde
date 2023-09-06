0164ee2805addfb7651216a310f1453971f56cbc
nestor772 <nestorjduhalde@gmail.com>
2023-09-06 19:25:20 -0300

    preproyecto2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/Navbar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemCount from "./components/ItemCount/ItemCount";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting={"Bienvenidos"} />
+      <ItemCount
+        initial={1}
+        stock={10}
+        onAdd={(quantity) => console.log("Cantidad agregada ", quantity)}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,4 +1,12 @@
+import { useState, useEffect } from "react";
+import { getProducts } from '../../asyncMock'
+import ItemList from '../ItemList/ItemList'
+
+
 const ItemListContainer = ({ greeting }) => {
+    const [products, setProducts] = useState ([])
+
+    useEffect(())
   return (
     <div>
       <h1>{greeting}</h1>
